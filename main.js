39383f87ce0ec8aa328ee831ecbfe26a83c5b24e
ibrahims168 <ibrahh881@gmail.com>
2022-01-25 17:21:02 +0200

    newjavascript

---
+++ main.js
@@ -0,0 +1,12 @@
+let birthYear = 1999
+let currentYear = 2022
+let age = currentYear - birthYear
+let ageInMonths = age * 12 
+let firstName = "Ibrahim"
+let lastName = "Sharif"
+let fulName = `${firstName} ${lastName}` 
+
+let greet=" hello I'm " + fulName + " and I'm " + age + "years old"
+console.log(greet);
+greet = `Hello I'm ${fulName} and I'm ${ageInMonths} months old`
+console.log(greet) ;
